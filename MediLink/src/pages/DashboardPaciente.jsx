33c5e954f5168971fafcdca670526783823fb699
juanchoalcoba <juanchoalcoba@gmail.com>
2024-10-16 17:16:59 -0300

    Component Register

---
+++ MediLink/src/pages/DashboardPaciente.jsx
@@ -0,0 +1,8 @@
+
+const DashboardPaciente = () => {
+  return (
+    <div className="h-screen">DashboardPaciente</div>
+  )
+}
+
+export default DashboardPaciente
--- MediLink/src/routes/Routers.jsx
+++ MediLink/src/routes/Routers.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
+import DashboardPaciente from "../pages/DashboardPaciente";
 
 
 const Routers = () => {
@@ -8,6 +9,7 @@ const Routers = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dashboardpaciente" element={<DashboardPaciente />} />
     </Routes>
   );
 };
