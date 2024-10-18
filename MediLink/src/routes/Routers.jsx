8fec86667dbe08a7b4374eb993bb21526cd009a2
MiltonOscarFlores <miltonoscarflores@gmail.com>
2024-10-18 17:27:38 -0300

    /feat login component and route

--- MediLink/src/routes/Routers.jsx
+++ MediLink/src/routes/Routers.jsx
@@ -2,14 +2,27 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
 import DashboardPaciente from "../pages/DashboardPaciente";
-
+import Login from "../pages/Login";
 
 const Routers = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dashboardpaciente" element={<DashboardPaciente />} />
+      <Route
+        path="/"
+        element={<Home />}
+      />
+      <Route
+        path="/register"
+        element={<Register />}
+      />
+      <Route
+        path="/dashboardpaciente"
+        element={<DashboardPaciente />}
+      />
+      <Route
+        path="/login"
+        element={<Login />}
+      />
     </Routes>
   );
 };
