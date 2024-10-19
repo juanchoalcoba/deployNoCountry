e1b05d4413d27d0e82cdf53b464f6409f64b9bd2
MiltonOscarFlores <miltonoscarflores@gmail.com>
2024-10-19 03:09:05 -0300

    /feat added sections cards and specialists to main page

--- MediLink/src/pages/Home.jsx
+++ MediLink/src/pages/Home.jsx
@@ -1,13 +1,17 @@
-import Hero from '../components/Hero'
-import Header from '../components/Header'
+import Hero from "../components/Hero";
+import Header from "../components/Header";
+import SectionCards from "../components/SectionCards";
+import SectionSpecialists from "../components/SectionSpecialists";
 
 const Home = () => {
   return (
     <div>
-        <Header />
-        <Hero />
+      <Header />
+      <Hero />
+      <SectionCards />
+      <SectionSpecialists />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
